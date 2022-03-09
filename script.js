b0ae2eabff6c7553b0ee40c37254182f32704df9
= <biernat521@gmail.com>
2022-03-09 21:00:01 +0100

    Add files

---
+++ script.js
@@ -0,0 +1,29 @@
+const toggleBtn = document.querySelector('.toggle-button');
+const menu = document.querySelector('ul')
+const firstInput = document.querySelector('.first-input')
+const secondInput = document.querySelector('.second-input')
+const resultArea = document.querySelector('.result')
+
+/*Menu*/ 
+const toggleMenu = () => {
+  menu.classList.toggle('active')
+  toggleBtn.classList.toggle('active-menu')
+}
+
+toggleBtn.addEventListener('click', toggleMenu)
+
+/*Result*/ 
+const calculate = () => {
+  const a = firstInput.value;
+  const b = secondInput.value;
+
+  let result;
+
+  result = 12*(a*b - a*b/5)
+
+  console.log(result)
+  resultArea.innerHTML = `${result.toFixed(2)} zł`;
+}
+
+secondInput.addEventListener('change', calculate)
+firstInput.addEventListener('change', calculate)
